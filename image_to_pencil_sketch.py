58b858c038f182d02f5bfe700e442494f63214b5
jeetra <68225568+jeetra@users.noreply.github.com>
2021-05-26 11:11:09 +0530

    Update image_to_pencil_sketch.py

--- image_to_pencil_sketch.py
+++ image_to_pencil_sketch.py
@@ -25,25 +25,3 @@ speak("File created and saved")
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-'''
-import scipy.ndimageblur_img = scipy.ndimage.filters.gaussian_filter(inverted_img,sigma=5)
-def dodge(front,back): result=front*255/(255-back)  result[result>255]=255 result[back==255]=255 return result.astype(‘uint8’)
-final_img= dodge(blur_img,gray_img)
-import matplotlib.pyplot as pltplt.imshow(final_img, cmap=”gray”)
-plt.imsave(‘img2.png’, final_img, cmap=’gray’, vmin=0, vmax=255)
-''''''
-import cv2
-image=cv2.imread("C:\\Users\\Jeet Raiwal\\Pictures\\Camera Roll\\IMG-20190628-WA0011.jpg")
-start_img.shape(196, 160, 30)
-#Y= 0.299 R + 0.587 G + 0.114 B
-import numpy as npdef 
-grayscale(rgb):return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
-gray_img = grayscale(start_img)
-inverted_img = 255-gray_img
-import scipy.ndimageblur_img = scipy.ndimage.filters.gaussian_filter(inverted_img,sigma=5)
-def dodge(front,back): result=front*255/(255-back)  result[result>255]=255 result[back==255]=255 return result.astype(‘uint8’)
-final_img= dodge(blur_img,gray_img)
-import matplotlib.pyplot as pltplt.imshow(final_img, cmap=”gray”)
-plt.imsave(‘img2.png’, final_img, cmap=’gray’, vmin=0, vmax=255)
-'''
